620ac162d3b84fa434f10d6ac9d8ed5b1e9abd7c
himanshu Nautiyal <himanshunautiyal3452@gmail.com>
2021-01-15 14:31:10 +0530

    add Postgre database and use secret key as an envrionmen variable in Haroku

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from forms import CreatePostForm, RegisterForm, Login, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy import ForeignKey
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -31,7 +32,7 @@ login_manager.init_app(app)
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
